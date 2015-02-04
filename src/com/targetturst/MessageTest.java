589857a947dc786fe36a0ac49383a34e82ece0b7
amtrindade <amtrindade@gmail.com>
2015-02-04 15:44:20 -0200

    Adicionando novo projeto

---
+++ src/com/targetturst/MessageTest.java
@@ -0,0 +1,44 @@
+package com.targetturst;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MessageTest {
+	
+	private WebDriver driver;
+
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		driver.get("http://treinoautomacao.hol.es/alertsconfirmation.html");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+
+	@Test
+	public void testManipulaAlert() {
+		
+		driver.findElement(By.xpath("//*[@id='alerta1']")).click();
+		Alert javascriptAlert = driver.switchTo().alert();
+	    System.out.println(javascriptAlert.getText()); 
+	    
+	    assertEquals("Olá!!! Eu sou um alerta!!!", javascriptAlert.getText());
+	    javascriptAlert.accept();
+
+	}
+
+}
